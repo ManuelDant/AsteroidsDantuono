c115e0af1868a9dd8e48f9e9d8290f0c15d1dbb8
ManuelDantuono <manuel1000d@gmail.com>
2022-09-14 23:14:50 -0300

    Agregado de librerias

--- Asteroids/main.cpp
+++ Asteroids/main.cpp
@@ -1,8 +1,39 @@
-#include <iostream>
+#include "raylib.h"
 
-using namespace std;
 
-int main() {
+int main(void)
+{
+   
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-	cout << "hola";
+    InitWindow(screenWidth, screenHeight, "Asteroids By: Manuel Dantuono");
+
+    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+
+    SetTargetFPS(60);               
+    
+
+    
+    while (!WindowShouldClose())    
+    {
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+
+        DrawCircleV(ballPosition, 50, MAROON);
+
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+
+    return 0;
 }
